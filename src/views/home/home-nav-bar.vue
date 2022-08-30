2e2569517bf02b8b352e29c2247a3216808debf1
xntzmk <hhb1298@gmail.com>
2022-08-30 21:28:02 +0800

    home页navbar封装和引入

---
+++ src/views/home/home-nav-bar.vue
@@ -0,0 +1,23 @@
+<script setup></script>
+
+<template>
+  <div class="nav-bar">
+    <div class="title">浅云旅途</div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.nav-bar {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 46px;
+  border-bottom: 1px solid #f2f2f2;
+
+  .title {
+    color: var(--theme-color);
+    font-size: 16px;
+    font-weight: 600;
+  }
+}
+</style>
