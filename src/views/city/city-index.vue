e45105b53e50ffce91f3ca4c11a19d0a60d38b8a
xntzmk <hhb1298@gmail.com>
2022-08-31 19:13:48 +0800

    city页搜索框和标签页完成，引入axios

--- src/views/city/city-index.vue
+++ src/views/city/city-index.vue
@@ -1,11 +1,39 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+// 搜索框功能
+const searchValue = ref('')
+const onCancel = () => {
+  router.go(-1)
+}
+
+// 标签页切换
+const tabActive = ref(0)
+</script>
 
 <template>
   <div class="city-index">
-    <h1>city-index</h1>
-    <template v-for="item in 100">
-      <div>item{{ item }}</div>
-    </template>
+    <!-- 搜索框 -->
+    <van-search
+      v-model="searchValue"
+      shape="round"
+      show-action
+      placeholder="城市/区域/位置"
+      @cancel="onCancel"
+    />
+
+    <!-- 标签页 -->
+    <van-tabs
+      v-model:active="tabActive"
+      color="var(--theme-color)"
+      line-height="2"
+    >
+      <van-tab title="国内·港澳台" />
+      <van-tab title="海外" />
+    </van-tabs>
   </div>
 </template>
 
